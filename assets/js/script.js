3626bcc3bcb679d1225ed7951d87ef4ea15cbde5
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-10-23 16:57:47 -0400

    added quiz questions choices and answers

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// array of quiz questions, choices & answers 
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "An HTML document must start with _____.",
+        choices: ["<!DOCTYPE html>", "<H1>", "<body>", "<head>"],
+        answer: "<!DOCTYPE html>"
+    },
+    {
+        title: "Which of the following is the correct way to format an element:",
+        choices: ["start tag, content, end tag", "start tag, content", "content, end tag", "content"],
+        answer: "start tag, content, end tag"
+    },
+    {
+        title: "An unordered list starts with the _____ tag.",
+        choices: ["<ol>", "<li>", "<ul>", "None of the above."],
+        answer: "<ul>"
+    },
+    {
+        title: "To reference a class in CSS you use which of the following characters:",
+        choices: ["#", "*", ".", "!"],
+        answer: "."
+    },
+    {
+        title: "CSS...",
+        choices: ["Specifies the layout of web pages.", "Defines the content of the web pages","Programs the behavior of web pages","All of teh above."],
+        answer: "Specifies the layout of the web pages."
+    },
+    {
+        title: "CSS selectors can be...",
+        choices: ["Simple Selectors", "Combinator Selectors", "Pseudo-class Selectors", "All of the above."],
+        answer: "All of the above."
+    },
+    {
+        title: "JavaScript code is inserted between _____ tags.",
+        choices: ["Java", "Header", "Script", "None of the above."],
+        answer: "Script"
+    },
+    {
+        title: "Which of the following is not a way to delcare a JavaScript Variable?",
+        choices: ["var","let", "const","declare"],
+        answer: "delcare"
+    },
+    {
+        title: "JavaScript arrays store...",
+        choices: ["Multiple Values", "One Value", "Elements","Tags"],
+        answer: "Multiple Values"
+    }
+]
+
+
+
+
+
